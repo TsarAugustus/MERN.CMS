47fc85e05a8ab30d5a6e9dda5f6e9b7ce7df01e2
John Ostap <ostap.john@gmail.com>
2021-02-03 15:08:35 -0800

    Added the server, and a concurrently script to run both

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
 
 function App() {
+  const [blogData, updateData] = useState();
+  useEffect(() => {
+    Axios.get('/testAPI').then(res => {
+      if(res.status === 200)
+        return res.data;
+    }).then(data => updateData(data.message));
+  });  
+
   return (
     <div className="App">
-      <h1>Hi</h1>
+      {blogData}
     </div>
   );
 }
